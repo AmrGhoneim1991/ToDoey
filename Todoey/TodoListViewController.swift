1fb6ef876282653c0dc932e25270fc2d26dc1ad6
AmrGhoneim1991 <amrghoneim1991@gmail.com>
2020-03-30 21:33:04 +0200

    add new item functionality complete

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["cell1" , "cell2" ,"cell3"]
-    
+    var itemArray = ["cell1" , "cell2" ,"cell3"]
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-// Mark - table view datasource methods
+      //Mark - table view datasource methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -29,7 +29,8 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
-//     Mark - Table view delegate methods
+     //MARk - Table view delegate methods
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
@@ -38,6 +39,25 @@ class TodoListViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    //MARK - add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+         var textField = UITextField()
+        let alert = UIAlertController(title: "Add Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            let item = textField.text
+            self.itemArray.append(item!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextFiled) in
+            alertTextFiled.placeholder = "create new item"
+            textField = alertTextFiled
+          
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
